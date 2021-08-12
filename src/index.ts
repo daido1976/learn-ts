d358ad2555422869bf7e6cd1107fa34e797774f4
daido1976 <daido1976@gmail.com>
2021-08-12 22:42:59 +0900

    Impl todo app

---
+++ src/index.ts
@@ -0,0 +1,83 @@
+import fs from "fs";
+import express from "express";
+const app = express();
+const port = 3000;
+const todoFilePath = "tmp/todo.json";
+
+type Todo = {
+  id: number;
+  title: string;
+  body: string;
+};
+
+type ReqTodo = {
+  title: string;
+  body: string;
+};
+
+app.use(express.json());
+
+app.get("/todos", (_req, res) => {
+  const todos: Todo[] = JSON.parse(fs.readFileSync(todoFilePath, "utf8"));
+  res.json(todos);
+});
+
+app.get("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todos: Todo[] = JSON.parse(fs.readFileSync(todoFilePath, "utf8"));
+  const todo = todos.find((todo) => todo.id === id);
+  todo
+    ? res.json(todo)
+    : res.status(404).send({ error: `Todo with id ${id} not found` });
+});
+
+app.post("/todos", (req, res) => {
+  const reqTodo: ReqTodo = req.body;
+  const todos: Todo[] = JSON.parse(fs.readFileSync(todoFilePath, "utf8"));
+  const newId = todos.length + 1;
+  const newTodo: Todo = { id: newId, ...reqTodo };
+  const newTodos = [...todos, newTodo];
+  fs.writeFileSync(todoFilePath, JSON.stringify(newTodos, null, 2));
+
+  res.json(newTodo);
+});
+
+app.patch("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const reqTodo: ReqTodo = req.body;
+  const todos: Todo[] = JSON.parse(fs.readFileSync(todoFilePath, "utf8"));
+  let updated = false;
+  let newTodo;
+  const newTodos = todos.map((todo) => {
+    if (todo.id === id) {
+      todo.title = reqTodo.title;
+      todo.body = reqTodo.body;
+      newTodo = todo;
+      updated = true;
+    }
+    return todo;
+  });
+
+  if (updated) {
+    fs.writeFileSync(todoFilePath, JSON.stringify(newTodos, null, 2));
+    res.json(newTodo);
+  } else {
+    res.status(404).send({ error: `Todo with id ${id} not found` });
+  }
+});
+
+app.delete("/todos/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const todos: Todo[] = JSON.parse(fs.readFileSync(todoFilePath, "utf8"));
+  const newTodos = todos.filter((todo) => todo.id !== id);
+  if (JSON.stringify(newTodos) !== JSON.stringify(todos)) {
+    fs.writeFileSync(todoFilePath, JSON.stringify(newTodos, null, 2));
+    res.json({});
+  } else {
+    res.status(404).send({ error: `Todo with id ${id} not found` });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
