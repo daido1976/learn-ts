94ffdfcf9d02025e094a1f02792026eee07da8f0
daido1976 <daido1976@gmail.com>
2023-05-02 16:09:50 +0900

    Refactor index.mjs

--- recognizer/src/index.mjs
+++ recognizer/src/index.mjs
@@ -2,8 +2,34 @@ import { getToken } from "./token.mjs";
 import { analyzeImage, extractTextFrom } from "./ocr.mjs";
 import { analyzeEntities, extractContactInfo } from "./nlp.mjs";
 
-const token = await getToken();
-const text = extractTextFrom(await analyzeImage(token));
-console.log(text);
-const result = extractContactInfo(await analyzeEntities(token, text));
-console.log(result);
+async function main() {
+  try {
+    const token = await getToken();
+    if (!token) {
+      console.error("Unable to get access token.");
+      return;
+    }
+
+    const visionResponse = await analyzeImage(token);
+    if (!visionResponse) {
+      console.error("Unable to analyze image.");
+      return;
+    }
+
+    const text = extractTextFrom(visionResponse);
+    console.log(text);
+
+    const nlResponse = await analyzeEntities(token, text);
+    if (!nlResponse) {
+      console.error("Unable to analyze entities.");
+      return;
+    }
+
+    const result = extractContactInfo(nlResponse);
+    console.log(result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+main();
