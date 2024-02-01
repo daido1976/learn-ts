345203f89f2316cc2e3fc0ea3f79b39ab484a90b
daido1976 <daido1976@gmail.com>
2024-02-01 17:22:18 +0900

    Revert "Improve comment-based injector to support multiple file paths and comment pairs"
    This reverts commit 3cdc6d9901d5dc6b1e92c229bc8e7c455e5a9df7.

--- utils/comment-based-injector.mjs
+++ utils/comment-based-injector.mjs
@@ -5,26 +5,19 @@ import process from "node:process";
 
 // ファイル名、対象のコメント、挿入したい文字列をオブジェクトで指定
 const entries = [
-  {
-    file: "your/file/path",
-    pairs: [
-      {
-        comment: "comment",
-        insert: "inserted text",
-      },
-    ],
-  },
+  { file: "file1.php", comment: "comment1", insert: "insert1" },
+  { file: "file2.php", comment: "comment2", insert: "insert2" },
+  { file: "file3.php", comment: "comment3", insert: "insert3" },
 ];
 
-entries.forEach((entry) => {
+entries.forEach(async (entry) => {
   const filePath = path.resolve(process.cwd(), entry.file);
-  const fileContent = fs.readFileSync(filePath, "utf8");
+  const fileContent = await fs.promises.readFile(filePath, "utf8");
   const lines = fileContent.split("\n");
 
-  const newLines = lines.flatMap((line) => {
-    const pair = entry.pairs.find((pair) => line.includes(pair.comment));
-    return pair ? [line, pair.insert] : [line];
-  });
+  const newLines = lines.flatMap((line) =>
+    line.includes(entry.comment) ? [line, entry.insert] : [line]
+  );
 
-  fs.writeFileSync(filePath, newLines.join("\n"), "utf8");
+  await fs.promises.writeFile(filePath, newLines.join("\n"), "utf8");
 });
