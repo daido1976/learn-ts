2385e99a76cea44f366d21384008ea8f2c54d733
daido1976 <daido1976@gmail.com>
2024-07-01 11:55:56 +0900

    Refactor with-libreoffice

--- excel-to-pdf/with-libreoffice.mjs
+++ excel-to-pdf/with-libreoffice.mjs
@@ -1,3 +1,4 @@
+// require: $ brew install --cask libreoffice
 import { execSync } from "node:child_process";
 import { promises as fs } from "node:fs";
 import path from "node:path";
@@ -5,18 +6,16 @@ import path from "node:path";
 /**
  * Convert an Excel file to a PDF file.
  * @param {string} inputPath - The path to the input Excel file.
- * @param {string} outputPath - The path to save the output PDF file.
+ * @param {string} outputDir - The directory to save the output PDF file.
  * @returns {Promise<void>} - A promise that resolves when the conversion is complete.
  */
-const convertExcelToPdf = async (inputPath, outputPath) => {
+const convertExcelToPdf = async (inputPath, outputDir) => {
   try {
     // Check if the input file exists
     await fs.access(inputPath);
 
     // Construct the command to run LibreOffice in headless mode
-    const command = `soffice --headless --convert-to pdf --outdir ${path.dirname(
-      outputPath
-    )} ${inputPath}`;
+    const command = `soffice --headless --convert-to pdf --outdir ${outputDir} ${inputPath}`;
 
     // Execute the command
     const result = execSync(command, { encoding: "utf-8" });
@@ -24,6 +23,10 @@ const convertExcelToPdf = async (inputPath, outputPath) => {
     console.log(result);
 
     // Verify if the output file has been created
+    const outputPath = path.join(
+      outputDir,
+      `${path.parse(inputPath).name}.pdf`
+    );
     await fs.access(outputPath);
 
     console.log(`Conversion successful: ${outputPath}`);
@@ -36,18 +39,15 @@ const convertExcelToPdf = async (inputPath, outputPath) => {
 // コマンドライン引数の処理
 const args = process.argv.slice(2);
 const inputPath = args[0];
+const outputDir = args[1] || process.cwd();
 
-if (!inputPath) {
-  console.error("Error: inputPath is required");
+if (!inputPath || !outputDir) {
+  console.error("Error: inputPath and outputDir are required");
   process.exit(1);
 }
 
-// 出力パスが指定されていない場合は、現在のディレクトリに保存
-const outputPath =
-  args[1] || path.join(process.cwd(), `${path.parse(inputPath).name}.pdf`);
-
 try {
-  await convertExcelToPdf(inputPath, outputPath);
+  await convertExcelToPdf(inputPath, outputDir);
   console.log("Excel to PDF conversion complete!");
 } catch (error) {
   console.error("Error converting Excel to PDF:", error);
